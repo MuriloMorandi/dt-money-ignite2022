b2882e9babc78a5717fb9f0b80c9ba2e5924cbeb
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2023-06-08 13:12:38 -0300

    Desenvolvimento do projeto

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+
+import { TransactionsProvider } from "./contexts/transactionsContext";
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Transactions } from './pages/transactions'
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <TransactionsProvider>
+        <Transactions/>
+      </TransactionsProvider>
+    </ThemeProvider>
+  )
+}
